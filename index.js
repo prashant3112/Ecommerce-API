84b39bcc220c7d29ad7a3c7b0b017f41d9e3b261
prashant <prashagupt@gmail.com>
2023-01-28 18:04:35 +0530

    final

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./config/connection");
+const Products = require("./modals/ECommerceSchema");
+const router = require("./routes/index");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// for environment variable
+
+const port = 8000;
+app.use(express.json());
+
+app.use("/products", router);
+app.get("/", (req, res) => {
+  res.send(
+    "<body style='background-color:cyan'><h1 style='text-align:center; font-size:50px'>Welcome to Ecommerce API </h1></body>"
+  );
+});
+app.get("*", (req, res) => {
+  res.send(
+    "<body style='background-color:cyan'><h1 style='text-align:center; font-size:50px'>ERROR 404 </h1></body>"
+  );
+});
+
+app.listen(port, () => {
+  console.log("my server is running at port No ", port);
+});
